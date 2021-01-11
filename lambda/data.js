e20665cf70e01048a72b61d51d1cd1e7872875a5
James Pratt <jamesgeorgepratt@gmail.com>
2021-01-11 12:24:26 -0500

    init commit

---
+++ lambda/data.js
@@ -0,0 +1,367 @@
+module.exports = {
+    
+    picks: [
+      {
+        "id": 112497546,
+        "league_name": "nba",
+        "play": "BOS +1.5 (Live)",
+        "game_id": 94388,
+        "competition_id": null,
+        "competitor_id": null,
+        "user_id": 1088940,
+        "type": "spread_away",
+        "period": "live",
+        "side_id": 164,
+        "value": 1.5,
+        "odds": -115,
+        "units": 1,
+        "units_net": 0.8696,
+        "money": 1,
+        "money_net": 0.8696,
+        "units_type": "money",
+        "result": "win",
+        "meta": {
+          "book_id": 76
+        },
+        "status": "settled",
+        "group_pick_id": null,
+        "updated_at": "2021-01-07T03:08:23.625Z",
+        "created_at": "2021-01-07T00:52:46.380Z",
+        "verified": false,
+        "tag": null,
+        "custom_pick_type": null,
+        "custom_pick_name": null,
+        "custom_pick_rules": null,
+        "settled_at": "2021-01-07T03:08:23.625Z",
+        "record_date": "2021-01-06T08:00:00.000Z",
+        "starts_at": "2021-01-07T00:30:00.000Z",
+        "ends_at": "2021-01-07T07:59:59.999Z",
+        "player_id": null,
+        "game": {
+          "id": 94388,
+          "status": "complete",
+          "real_status": "complete",
+          "status_display": "Final",
+          "start_time": "2021-01-07T00:30:00.000Z",
+          "away_team_id": 164,
+          "home_team_id": 160,
+          "league_name": "nba",
+          "season": 2020,
+          "attendance": 0,
+          "coverage": "full",
+          "is_free": false,
+          "trending": false,
+          "teams": [
+            {
+              "id": 164,
+              "abbr": "BOS",
+              "full_name": "Boston Celtics",
+              "display_name": "Celtics",
+              "short_name": "Celtics",
+              "location": "Boston",
+              "logo": "https://static.sprtactn.co/teamlogos/nba/100/bos.png",
+              "primary_color": "008348",
+              "secondary_color": "BB9753",
+              "division_type": "ATLANTIC",
+              "conference_type": "EASTERN"
+            },
+            {
+              "id": 160,
+              "abbr": "MIA",
+              "full_name": "Miami Heat",
+              "display_name": "Heat",
+              "short_name": "Heat",
+              "location": "Miami",
+              "logo": "https://static.sprtactn.co/teamlogos/nba/100/mia.png",
+              "primary_color": "98002E",
+              "secondary_color": "F9A01B",
+              "division_type": "SOUTHEAST",
+              "conference_type": "EASTERN"
+            }
+          ],
+          "last_play": {
+            "text": "End of 4th Quarter.",
+            "type": "endperiod",
+            "clock": "0:00",
+            "home_win_pct": null,
+            "over_win_pct": null,
+            "home_spread_win_pct": null
+          },
+          "boxscore": {
+            "clock": "00:00",
+            "period": 4,
+            "outcome": {
+              "times_tied": 9,
+              "lead_changes": 10
+            },
+            "duration": "00:00",
+            "linescore": [
+              {
+                "id": 1,
+                "abbr": "1",
+                "display_name": "1st",
+                "full_name": "1st Quarter",
+                "away_points": 34,
+                "home_points": 26
+              },
+              {
+                "id": 2,
+                "abbr": "2",
+                "display_name": "2nd",
+                "full_name": "2nd Quarter",
+                "away_points": 19,
+                "home_points": 19
+              },
+              {
+                "id": 3,
+                "abbr": "3",
+                "display_name": "3rd",
+                "full_name": "3rd Quarter",
+                "away_points": 27,
+                "home_points": 35
+              },
+              {
+                "id": 4,
+                "abbr": "4",
+                "display_name": "4th",
+                "full_name": "4th Quarter",
+                "away_points": 27,
+                "home_points": 25
+              }
+            ],
+            "total_away_points": 107,
+            "total_home_points": 105,
+            "total_away_firsthalf_points": 53,
+            "total_home_firsthalf_points": 45,
+            "total_away_secondhalf_points": 54,
+            "total_home_secondhalf_points": 60
+          }
+        },
+        "current_projection": null,
+        "actual_value": null,
+        "trend": null
+      },
+      {
+        "id": 112497582,
+        "league_name": "nba",
+        "play": "Over 218 (Live)",
+        "game_id": 94388,
+        "competition_id": null,
+        "competitor_id": null,
+        "user_id": 1088940,
+        "type": "over",
+        "period": "live",
+        "side_id": null,
+        "value": 218,
+        "odds": -115,
+        "units": 1,
+        "units_net": -1,
+        "money": 1,
+        "money_net": -1,
+        "units_type": "money",
+        "result": "loss",
+        "meta": {
+          "book_id": 15
+        },
+        "status": "settled",
+        "group_pick_id": null,
+        "updated_at": "2021-01-07T03:08:23.625Z",
+        "created_at": "2021-01-07T00:52:54.096Z",
+        "verified": false,
+        "tag": null,
+        "custom_pick_type": null,
+        "custom_pick_name": null,
+        "custom_pick_rules": null,
+        "settled_at": "2021-01-07T03:08:23.625Z",
+        "record_date": "2021-01-06T08:00:00.000Z",
+        "starts_at": "2021-01-07T00:30:00.000Z",
+        "ends_at": "2021-01-07T07:59:59.999Z",
+        "player_id": null,
+        "game": {
+          "id": 94388,
+          "status": "complete",
+          "real_status": "complete",
+          "status_display": "Final",
+          "start_time": "2021-01-07T00:30:00.000Z",
+          "away_team_id": 164,
+          "home_team_id": 160,
+          "league_name": "nba",
+          "season": 2020,
+          "attendance": 0,
+          "coverage": "full",
+          "is_free": false,
+          "trending": false,
+          "teams": [
+            {
+              "id": 164,
+              "abbr": "BOS",
+              "full_name": "Boston Celtics",
+              "display_name": "Celtics",
+              "short_name": "Celtics",
+              "location": "Boston",
+              "logo": "https://static.sprtactn.co/teamlogos/nba/100/bos.png",
+              "primary_color": "008348",
+              "secondary_color": "BB9753",
+              "division_type": "ATLANTIC",
+              "conference_type": "EASTERN"
+            },
+            {
+              "id": 160,
+              "abbr": "MIA",
+              "full_name": "Miami Heat",
+              "display_name": "Heat",
+              "short_name": "Heat",
+              "location": "Miami",
+              "logo": "https://static.sprtactn.co/teamlogos/nba/100/mia.png",
+              "primary_color": "98002E",
+              "secondary_color": "F9A01B",
+              "division_type": "SOUTHEAST",
+              "conference_type": "EASTERN"
+            }
+          ],
+          "last_play": {
+            "text": "End of 4th Quarter.",
+            "type": "endperiod",
+            "clock": "0:00",
+            "home_win_pct": null,
+            "over_win_pct": null,
+            "home_spread_win_pct": null
+          },
+          "boxscore": {
+            "clock": "00:00",
+            "period": 4,
+            "outcome": {
+              "times_tied": 9,
+              "lead_changes": 10
+            },
+            "duration": "00:00",
+            "linescore": [
+              {
+                "id": 1,
+                "abbr": "1",
+                "display_name": "1st",
+                "full_name": "1st Quarter",
+                "away_points": 34,
+                "home_points": 26
+              },
+              {
+                "id": 2,
+                "abbr": "2",
+                "display_name": "2nd",
+                "full_name": "2nd Quarter",
+                "away_points": 19,
+                "home_points": 19
+              },
+              {
+                "id": 3,
+                "abbr": "3",
+                "display_name": "3rd",
+                "full_name": "3rd Quarter",
+                "away_points": 27,
+                "home_points": 35
+              },
+              {
+                "id": 4,
+                "abbr": "4",
+                "display_name": "4th",
+                "full_name": "4th Quarter",
+                "away_points": 27,
+                "home_points": 25
+              }
+            ],
+            "total_away_points": 107,
+            "total_home_points": 105,
+            "total_away_firsthalf_points": 53,
+            "total_home_firsthalf_points": 45,
+            "total_away_secondhalf_points": 54,
+            "total_home_secondhalf_points": 60
+          }
+        },
+        "current_projection": null,
+        "actual_value": null,
+        "trend": null
+      }, {
+        "id": 112596199,
+        "league_name": "nfl",
+        "play": "Over 54.5",
+        "game_id": 94946,
+        "competition_id": null,
+        "competitor_id": null,
+        "user_id": 1088940,
+        "type": "over",
+        "period": "game",
+        "side_id": null,
+        "value": 54.5,
+        "odds": -111,
+        "units": 1,
+        "units_net": 0,
+        "money": 1,
+        "money_net": 0,
+        "units_type": "money",
+        "result": "pending",
+        "meta": {
+          "book_id": 15
+        },
+        "status": "published",
+        "group_pick_id": null,
+        "updated_at": "2021-01-07T17:56:40.094Z",
+        "created_at": "2021-01-07T17:56:40.094Z",
+        "verified": false,
+        "tag": null,
+        "custom_pick_type": null,
+        "custom_pick_name": null,
+        "custom_pick_rules": null,
+        "settled_at": null,
+        "record_date": null,
+        "starts_at": "2021-01-10T18:05:00.000Z",
+        "ends_at": "2021-01-11T07:59:59.999Z",
+        "player_id": null,
+        "game": {
+          "id": 94946,
+          "status": "scheduled",
+          "real_status": "scheduled",
+          "status_display": null,
+          "start_time": "2021-01-10T18:05:00.000Z",
+          "away_team_id": 131,
+          "home_team_id": 135,
+          "league_name": "nfl",
+          "season": 2020,
+          "attendance": 0,
+          "coverage": "full",
+          "is_free": false,
+          "trending": false,
+          "teams": [
+            {
+              "id": 135,
+              "abbr": "TEN",
+              "full_name": "Tennessee Titans",
+              "display_name": "Titans",
+              "short_name": "Titans",
+              "location": "Tennessee",
+              "logo": "https://static.sprtactn.co/teamlogos/nfl/100/ten.png",
+              "primary_color": "4495D1",
+              "secondary_color": "002A5C",
+              "division_type": "SOUTH",
+              "conference_type": "AFC"
+            },
+            {
+              "id": 131,
+              "abbr": "BAL",
+              "full_name": "Baltimore Ravens",
+              "display_name": "Ravens",
+              "short_name": "Ravens",
+              "location": "Baltimore",
+              "logo": "https://static.sprtactn.co/teamlogos/nfl/100/bal.png",
+              "primary_color": "241773",
+              "secondary_color": "9E7C0C",
+              "division_type": "NORTH",
+              "conference_type": "AFC"
+            }
+          ]
+        },
+        "current_projection": null,
+        "actual_value": null,
+        "trend": null
+      }]
+    
+};
